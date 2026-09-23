053b2e3e25a46ca79c5351408abc2b208e34c252
siddhisaindane <zcoer_ra1150_2025_26@zealeducation.com>
2026-09-23 23:58:39 +0530

    Add initial Program3.c with project status output

---
+++ Program3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("===============================\n");
+    printf("Project: myfirstproject\n");
+    printf("Status: Ready to push to GitHub\n");
+    printf("===============================\n");
+    return 0;
+}
