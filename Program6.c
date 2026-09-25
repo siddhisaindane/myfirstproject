fe0cc85e030324e9afb24dc306bd88f76f53d856
siddhisaindane <zcoer_ra1150_2025_26@zealeducation.com>
2026-09-25 12:16:35 +0530

    Add Program6.c with Hello World example

---
+++ Program6.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Library for standard input and output functions
+
+int main() {
+    // printf() displays the string inside quotation marks
+    printf("Hello, World!\n");
+    
+    return 0; // Signals that the program executed successfully
+}
